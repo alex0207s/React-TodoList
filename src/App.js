b6dbcb054fe861e9593b3a8c8220a10947d6b0ce
alex0207 <alex640702070829@gmail.com>
2022-08-18 00:57:52 +0800

    fix: use uuid as todo item id to avoid deleting wrong todo item

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { useState } from 'react';
+import { v4 } from 'uuid';
 import './App.css';
 
 function Items(props) {
   const { todo, setTodo, activeTab } = props;
 
-  const setFinished = (content) => {
-    const targetIndex = todo.findIndex((x) => x.content == content);
+  const setFinished = (id) => {
+    const targetIndex = todo.findIndex((x) => x.id == id);
 
-    const todo2 = [...todo];
-    todo2[targetIndex].finished = !todo2[targetIndex].finished;
+    const updateTodo = [...todo];
+    updateTodo[targetIndex].finished = !updateTodo[targetIndex].finished;
 
-    setTodo(todo2);
+    setTodo(updateTodo);
+  };
+
+  const removeTodoItem = (id) => {
+    setTodo(todo.filter((item) => item.id != id));
   };
 
   let showTodo = [];
@@ -23,30 +28,20 @@ function Items(props) {
     showTodo = [...todo].filter((item) => item.finished === true);
   }
 
-  return showTodo.map((todo_item, i) => (
-    <li key={i}>
+  return showTodo.map((todo_item) => (
+    <li key={todo_item.id}>
       <label className="todoList_label">
         <input
           className="todoList_input"
           type="checkbox"
-          checked={todo_item.finished}
-          onClick={(e) => {
-            setFinished(todo_item.content);
-            console.log(e.target.value);
+          onClick={() => {
+            setFinished(todo_item.id);
           }}
         />
         <span>{todo_item.content}</span>
       </label>
-      <a href="#">
-        <i
-          className="fa fa-times"
-          onClick={() => {
-            console.log(todo_item.content);
-            setTodo(
-              [...todo].filter((item) => item.content !== todo_item.content)
-            );
-          }}
-        ></i>
+      <a href="#" onClick={() => removeTodoItem(todo_item.id)}>
+        <i className="fa fa-times"></i>
       </a>
     </li>
   ));
@@ -54,32 +49,28 @@ function Items(props) {
 
 function InputBox(props) {
   const { todo, setTodo } = props;
-  const [newTodo, setNewTodo] = useState({ content: '', finished: false });
 
-  const addTodo = () => {
-    if (newTodo.content == '') {
+  const addTodo = (e) => {
+    if (e.target.input.value == '') {
       alert('請輸入待辦事項!');
       return;
     }
 
-    setTodo([...todo, newTodo]);
-    setNewTodo({ content: '', finished: false });
+    setTodo([
+      ...todo,
+      { id: v4(), content: e.target.input.value, finished: false },
+    ]);
+
+    e.target.input.value = '';
   };
 
   return (
-    <div className="inputBox">
-      <input
-        type="text"
-        placeholder="請輸入待辦事項"
-        value={newTodo.content}
-        onChange={(e) => {
-          setNewTodo({ content: e.target.value, finished: false });
-        }}
-      />
-      <a href="#">
-        <i className="fa fa-plus" onClick={addTodo}></i>
-      </a>
-    </div>
+    <form className="inputBox" onSubmit={addTodo}>
+      <input name="input" type="text" placeholder="請輸入待辦事項" />
+      <button type="submit">
+        <i className="fa fa-plus"></i>
+      </button>
+    </form>
   );
 }
 
@@ -105,26 +96,32 @@ function TodoListTab(props) {
 function App() {
   const [todo, setTodo] = useState([
     {
+      id: v4(),
       content: '把冰箱發霉的檸檬拿去丟',
       finished: false,
     },
     {
+      id: v4(),
       content: '打電話叫媽媽匯款給我',
       finished: false,
     },
     {
+      id: v4(),
       content: '整理電腦資料',
       finished: false,
     },
     {
+      id: v4(),
       content: '繳電費水費瓦斯費',
       finished: false,
     },
     {
+      id: v4(),
       content: '約vicky禮拜三泡溫泉',
       finished: false,
     },
     {
+      id: v4(),
       content: '約 ada 禮拜四吃晚餐',
       finished: false,
     },
